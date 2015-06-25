b1f48a63fa9ab906a6a2eb55b59840af17fc3015
Paulo Garcia <pgarcia@localhost.localdomain>
2015-06-24 22:22:26 -0300

    Wired collection into 'postsList' template.

--- server/fixture.js
+++ server/fixture.js
@@ -4,14 +4,16 @@ title: 'Introducing Telescope',
 author: 'Sacha Greif',
 url: 'http://sachagreif.com/introducing-telescope/'
 });
+
 Posts.insert({
 title: 'Meteor',
 author: 'Tom Coleman',
 url: 'http://meteor.com'
 });
-}
+
 Posts.insert({
 title: 'The Meteor Book',
 author: 'Tom Coleman',
 url: 'http://themeteorbook.com'
 });
+}
